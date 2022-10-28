60b23345c42f473fda2092f1e33d0b0a69ff7384
Cleo <cleotran05@gmail.com>
2022-10-28 19:58:21 -0400

    tkgjfbfmhkj half way

---
+++ MEANY_BIRD_BB+CAMILO/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverCanvas;
+    public GameObject scoreCanvas;
+    public GameObject spawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        scoreCanvas.SetActive(true);
+        spawner.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        gameOverCanvas.SetActive(true);
+        scoreCanvas.SetActive(false);
+        spawner.SetActive(false);
+        Time.timeScale = 0;
+    }
+}
